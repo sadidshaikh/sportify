a1acc0c98005513861346f2ad70a0fb20a939b36
sadid <shaikhsadid431@gmail.com>
2025-02-16 23:09:02 +0530

    Initial commit

---
+++ src/test/kotlin/com/example/sportify/SportifyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.sportify
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SportifyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
